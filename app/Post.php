259c4e2a94dbf620f7a958a395f838102d7438c7
mostafaAli122 <mostafaali122@outlook.com>
2018-11-29 10:39:56 +0200

    Using softDelete To Trash Posts

--- app/Post.php
+++ app/Post.php
@@ -3,11 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
+
 
 class Post extends Model
 {
+    use softDeletes;
     protected $fillable=['title','content','category_id','featured'];
     
+    protected $dates=['deleted_at'];
     public function category(){
         return $this->belongsTo('App\Category');
     }
